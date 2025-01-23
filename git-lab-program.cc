cb8969812ba24791487edc321ce03cff9aefe489
PLOB4TIMEWIZARD <TRUCKboatTRUCK007@outlook.com>
2025-01-22 20:33:55 -0500

    lab code

---
+++ git-lab-program.cc
@@ -0,0 +1,15 @@
+// file: git-lab-program.cc
+// author: Christian Thompson'
+// Date: 1/22/25
+// Description: lab 2 code
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello git!!!" << endl;
+    return(0);
+}
